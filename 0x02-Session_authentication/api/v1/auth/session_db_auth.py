58d2a0b99a8fca2dd15cea000afd0d8d90527d00
TrudyB-T <drtrudybannerman@gmail.com>
2024-03-21 16:11:14 +0000

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+Define class SessionDButh
+"""
+
+from flask import request
+from datetime import datetime, timedelta
+
+from models.user_session import UserSession
+from .session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    """
+    Definition of SessionDBAuth class that persists session data
+    in a database
+    """
+
+    def create_session(self, user_id=None) -> str:
+        """
+        Create a Session ID for a user_id
+        Args:
+           user_id (str): user id
+        """
+        session_id = super().create_session(user_id)
+        if type(session_id) == str:
+            kwargs = {
+                'user_id': user_id,
+                'session_id': session_id,
+            }
+            user_session = UserSession(**kwargs)
+            user_session.save()
+            return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """
+        Returns a user ID based on a session ID
+        Args:
+            session_id (str): session ID
+        Return:
+            user id or None if session_id is None or not a string
+        """
+        try:
+            sessions = UserSession.search({'session_id': session_id})
+        except Exception:
+            return None
+        if len(sessions) <= 0:
+            return None
+        cur_time = datetime.now()
+        time_span = timedelta(seconds=self.session_duration)
+        exp_time = sessions[0].created_at + time_span
+        if exp_time < cur_time:
+            return None
+        return sessions[0].user_id
+
+    def destroy_session(self, request=None) -> bool:
+        """
+        Destroy a UserSession instance based on a
+        Session ID from a request cookie
+        """
+        session_id = self.session_cookie(request)
+        try:
+            sessions = UserSession.search({'session_id': session_id})
+        except Exception:
+            return False
+        if len(sessions) <= 0:
+            return False
+        sessions[0].remove()
+        return True
